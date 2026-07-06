076e4da581c5b9b437fe11796cb901e646d32416
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-07-06 14:57:52 -0600

    Removed a semicolon
    It was causing an error in the browser

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ Rounding: Nearest thousandth (ex. .343)
 const response = fetch('https://api.bigballsdata.com/v1/players/{player_id}/stats?sport=baseball', {
   headers: {
     Authorization: "Bearer bbs_live_00000FQfo2ituNAqIBd98eazIP9M8HnT90MaSwLOBcgFMR5E", //security key was not kept secret because it is a free plan
-    Accept: "application/json";
+    Accept: "application/json"
   }
 })
 const data = response.json();
