6a88f65e38a92813af8d52d6e1dce7353ea73fdc
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-07-08 15:08:52 -0600

    Added function to be able to switch seasons

--- script.js
+++ script.js
@@ -1,13 +1,86 @@
 /* Stats explained:
+Earned Run Average (ERA): The average number of runs a pitcher gives up that is charged against him every 9 innings. An "earned" run is any run a pitcher gives up
+that reached base while they were pitching and did not reach due to an error, including errors charged against the pitcher. For example, if a routine ground ball
+is hit to the shortstop who then throws it over the first baseman's head, an error will be charged against the shortstop and if the run scores, it is not an ER.
+In addition, if a new pitcher enters the game while runners are on base, any runners that score that were already on base are charged to the previous pitcher.
+ERA Formula: (ER * 9) / IP
+Rounding: Nearest hundredth (ex. 3.15)
 Batting Average (AVG/BA): The percentage of hits per at bat. An "at bat" or AB is defined not by the amount of times the hitter steps up to the plate. It excludes
 walks, hit-by-pitches, sacrifice flies (flyouts that score a runner), and sacrifice bunts (a bunt that moves a runner over. Will not count if hitter was bunting
 for a hit, determined by the scorer).
 AVG Formula: H/AB
 Rounding: Nearest thousandth (ex. .343) 
 */
+let eraRank = 1;
 let avgRank = 1;
+let stat = "era";
 let colorNonQualifiedPlayers = true;
-async function getAvgData(season){
+async function getERAData(season) {
+        stat = "era";
+        let changeERATab = document.getElementById("eraTab"); //makes ERA tab look selected
+        changeERATab.style.backgroundColor = 'white';
+        changeERATab.style.border = '2px solid black';
+        let changeAvgTab = document.getElementById("avgTab"); //makes avg tab not selected
+        changeAvgTab.style.backgroundColor = 'gray';
+        changeAvgTab.style.border = '1px solid black';
+        const playerAPI = await fetch("https://statsapi.mlb.com/api/v1/stats?stats=season&group=pitching&playerPool=ALL&sportIds=1&season=" + season + "&limit=5000");
+        const teamAPI = await fetch ("https://statsapi.mlb.com/api/v1/teams/stats?stats=season&group=pitching&season=" + season + "&sportIds=1");
+        const pData = await playerAPI.json();
+        const tData = await teamAPI.json();
+        const teams = tData.stats[0].splits;
+        const players = pData.stats[0].splits;
+        let preAdjustmentERA = " ";
+        for (let i = 0; i < players.length; i++){
+            for (let j = 0; j <  30; j++){ //find player's team's games played for accurate minimum innings count
+                    if (players[i].team.id === teams[j].team.id){
+                            var minimumInnings = teams[j].stat.gamesPlayed; //var used for function scope
+                            break;
+                    }
+            }    
+            if (players[i].stat.inningsPitched >= minimumInnings){ //do not adjust qualified players
+                adjustedERA = (players[i].stat.era * 1).toFixed(2); //converts to accurate formatting e.g. 3 -> 3.00
+                players[i].adjustedERA = adjustedERA; //still uses adjustedERA so they can be compared against non-qualifiers
+                players[i].preAdjustmentERA = " ";
+                players[i].isQualified = true;
+            }
+            else if (players[i].stat.inningsPitched < minimumInnings){ //adjustment for non-qualified players
+                const modifiedERTotal = players[i].stat.earnedRuns + (minimumInnings - players[i].stat.inningsPitched);
+                let adjustedERA = (modifiedERTotal * 9) / minimumInnings;
+                adjustedERA = Math.round(adjustedERA * 100) / 100; //rounds to nearest hundredth
+                adjustedERA = (adjustedERA * 1).toFixed(2); //converts to accurate formatting e.g. 3 -> 3.00
+                players[i].adjustedERA = adjustedERA;
+                players[i].preAdjustmentERA = ", adjusted from: " + players[i].stat.era;
+                players[i].isQualified = false;
+            }
+        }
+        for (let i = 0; i < players.length; i++){ //increase rank if era is higher than other player
+            if (i > 0 && players[i].adjustedERA > players[i - 1].adjustedERA){
+                eraRank++;
+            }
+            players.sort((a, b) => a.adjustedERA - b.adjustedERA);
+            for (let i = 0; i < 20; i++) {
+                const changeRank = document.getElementById("rank" + (i + 1))
+                changeRank.textContent = players[i].player.fullName + ", ERA: " + players[i].adjustedERA + players[i].preAdjustmentERA;
+                if (players[i].isQualified === false && colorNonQualifiedPlayers === true){
+                        changeRank.style.color = "red"; //changes non-qualified players to red
+                }
+                if (players[i].isQualified === true){
+                        changeRank.style.color = "black"; //when changing from avg to ERA, reset qualified players to black
+                }
+                if (colorNonQualifiedPlayers === false){
+                        changeRank.style.color = "black"; //reset all players to black
+                }
+            }
+        }
+}
+async function getAvgData(season){ //uses same structure as getERAData, but with avg
+stat = "avg";
+        let changeERATab = document.getElementById("eraTab"); //makes avg tab look selected
+        let changeAvgTab = document.getElementById("avgTab"); //makes ERA tab not selected
+        changeAvgTab.style.backgroundColor = 'white';
+        changeAvgTab.style.border = '2px solid black';
+        changeERATab.style.backgroundColor = 'gray';
+        changeERATab.style.border = '1px solid black';
         const playerAPI = await fetch("https://statsapi.mlb.com/api/v1/stats?stats=season&group=hitting&playerPool=ALL&sportIds=1&season=" + season + "&limit=5000");
         const teamAPI = await fetch ("https://statsapi.mlb.com/api/v1/teams/stats?stats=season&group=hitting&season=" + season + "&sportIds=1");
         const pData = await playerAPI.json();
@@ -60,12 +133,22 @@ async function getAvgData(season){
 function changeQualifiedPlayerRule(){
         if (colorNonQualifiedPlayers === true){
                 colorNonQualifiedPlayers = false;
-                getAvgData(new Date().getFullYear()); //retriggers getAvgData with current year
+                if (stat === "avg"){
+                        getAvgData(new Date().getFullYear()); //retriggers getAvgData with current year
+                }
+                if (stat === "era"){
+                        getERAData(new Date().getFullYear());
+                }
                 return;
         }
         if (colorNonQualifiedPlayers === false){
                 colorNonQualifiedPlayers = true;
-                getAvgData(new Date().getFullYear()); //retriggers getAvgData with current year
+                if (stat === "avg"){
+                        getAvgData(new Date().getFullYear()); //retriggers getAvgData with current year
+                }
+                if (stat === "era"){
+                        getERAData(new Date().getFullYear());
+                }
                 return;
         }
 }
@@ -74,4 +157,14 @@ function switchToNL(){
 }
 function switchToAL(){
     
+}
+function processInput(){
+        const seasonInputElement = document.getElementById("seasonInput");
+        const season = seasonInputElement.value;
+        if (stat === "avg"){
+                getAvgData(season);
+        }
+        if (stat === "era"){
+                getERAData(season);
+        }
 }
