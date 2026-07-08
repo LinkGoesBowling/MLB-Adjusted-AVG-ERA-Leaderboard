b40f1f212b5387abd8ed0bc9e240177bc3968596
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-07-08 12:14:42 -0600

    Added console.log statement

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ async function getERAData(season) {
                     }
                     if (players[i].team.id === teams[i].team.id){
                             var minimumInnings = teams[i].stat.gamesPlayed; //var used for function scope
+                            console.log(players[i].player.fullName + " minimumInnings: " + teams[i].stat.gamesPlayed);
                             return;
                     }
             }    
