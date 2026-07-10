7d8dd160cc717892c51dda8539dd594e90048e79
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-07-09 20:01:07 -0600

    Fix ol1 can't be accessed before variable declaration

--- script.js
+++ script.js
@@ -164,6 +164,7 @@ async function getAvgData(season){ //uses same structure as getERAData, but with
                 }
             }
             players.sort((a, b) => b.adjustedAvg - a.adjustedAvg);
+                let ol1 = document.getElementById('playerRanks');
         for (let i = ol1.children.length; i < playersShown; i++) {
                 if (ol1.children.length < playersShown){ //change to half of playersShown for multiple rows
                         const createRanks = document.createElement('li'); //create new li elements and add them to the ol
