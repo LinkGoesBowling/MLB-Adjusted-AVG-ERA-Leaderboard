583f58286c19baef4197b28ea23e0a7244312111
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-07-09 18:53:19 -0600

    Find NL/AL filtering issue

--- script.js
+++ script.js
@@ -159,8 +159,8 @@ async function getAvgData(season){ //uses same structure as getERAData, but with
                 avgRank++;
             }
             players.sort((a, b) => b.adjustedAvg - a.adjustedAvg);
-            const ol1 = document.getElementById('playerRanks');
-            for (let i = ol1.children.length; i < playersShown; i++) {
+            for (let i = 0; i < playersShown; i++) {
+                const ol1 = document.getElementById('playerRanks');
                 if (ol1.children.length < playersShown){ //change to half of playersShown for multiple rows
                         const createRanks = document.createElement('li'); //create new li elements and add them to the ol
                         createRanks.classList.add('rank' + (i + 1 + (playersShown - 20))); //add class
